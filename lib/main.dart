874f631eb9befbd360f74d30717b66d286255333
engislamalabd <islam.abd.4321@outlook.com>
2024-05-06 21:22:01 +0300

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:apitesting/cache/cache_helper.dart';
+import 'package:apitesting/core/api/dio-consumer.dart';
+import 'package:apitesting/cubit/user_cubit.dart';
+import 'package:apitesting/screens/sign_in_screen.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  CacheHelper().init();
+  runApp(
+    BlocProvider(
+      create: (context) => UserCubit(DioConsumer(dio: Dio())),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignInScreen(),
+    );
+  }
+}
